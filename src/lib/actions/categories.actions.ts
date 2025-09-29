cfcf50c9b1847c3ff89179a90e7d7a4ed6ead8c2
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2025-09-29 23:41:11 +0800

    feat: add createCategory and createProject functions for category and project management

--- src/lib/actions/categories.actions.ts
+++ src/lib/actions/categories.actions.ts
@@ -1,3 +1,6 @@
+'use server'
+
+import { revalidatePath } from "next/cache";
 import { prisma } from "../prisma";
 
 export async function getAllCategories() {
@@ -12,5 +15,25 @@ export async function getAllCategories() {
   } catch (error) {
     console.error("Failed to fetch projects:", error);
     throw new Error("Failed to fetch projects.");
+  }
+}
+
+export async function createCategory(title: string) {
+  try {
+    if (!title) {
+      return { success: false, error: "Title is required." };
+    }
+
+    const newCategory = await prisma.category.create({
+      data: {
+        title,
+      },
+    });
+
+    revalidatePath("/admin");
+    return { success: true, category: newCategory };
+  } catch (error) {
+    console.error("Failed to create category:", error);
+    return { success: false, error: "Failed to create category." };
   }
 }
--- src/lib/actions/project.actions.ts
+++ src/lib/actions/project.actions.ts
@@ -1,4 +1,14 @@
+'use server'
+
 import { prisma } from "../prisma";
+import { revalidatePath } from "next/cache";
+
+type ProjectCreateInput = {
+  title: string;
+  description?: string | null;
+  image: string;
+  categoryId: number;
+};
 
 export async function getAllProjects() {
   try {
@@ -14,5 +24,25 @@ export async function getAllProjects() {
   } catch (error) {
     console.error("Failed to fetch projects:", error);
     throw new Error("Failed to fetch projects.");
+  }
+}
+
+export async function createProject(data: ProjectCreateInput) {
+  try {
+    const newProject = await prisma.project.create({
+      data: {
+        title: data.title,
+        description: data.description,
+        image: data.image,
+        categoryId: data.categoryId,
+      },
+    });
+
+    revalidatePath("/admin");
+
+    return { success: true, project: newProject };
+  } catch (error) {
+    console.error("Failed to create project:", error);
+    return { success: false, error: "Failed to create project." };
   }
 }
