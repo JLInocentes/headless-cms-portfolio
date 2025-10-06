5fca33434adfdad141786dfedba2e1d678a4b90e
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2025-10-06 22:42:32 +0800

    feat: update Navbar styles for improved layout and visual consistency

--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -4,9 +4,9 @@ import { teachers } from "../fonts";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between items-center p-[20] px-[100]">
-      <h1 className={`text-6xl font-semibold${teachers.className}`}>EULDRIQ</h1>
-      <div className="h-full flex align-center gap-16">
+    <div className="w-full h-[138px]  px-[123px] flex justify-between items-center  bg-gradient-to-t from-[#000000] via-[#191919] to-[#343434] shadow-[0_4px_100px_30px_#000000]">
+      <h1 className={`text-[45px] font-p400] ${teachers.className}`}>EULDRIQ</h1>
+      <div className="h-full flex items-center gap-16">
         {NAV_LINKS.map((link, index) => (
           <Link key={index} href={link.href} className={`font-bold text-xl`}>
             {link.name}
